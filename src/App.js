ec58da6d6041b7f0d419705c8437ee0a4a055f16
Alexander Reynoso Я <61547337+EAR1610@users.noreply.github.com>
2021-05-17 11:41:39 -0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React, { Fragment, useState } from 'react'
+import Header from './componentes/Header';
+import Formulario from './componentes/Formulario';
+import Mensaje from './componentes/Mensaje';
+import Resultado from './componentes/Resultado';
+import Spinner from './componentes/Spinner';
+
+function App() {
+
+  //Definir el state
+  const [cantidad, guardarCantidad] = useState(0);
+  const [plazo, guardarPlazo] = useState('');
+  const [total, guardarTotal] = useState(0);
+  const [cargando, guardarCargando] = useState(false);
+
+  let componente; 
+
+  if( cargando ){
+    componente = <Spinner />
+  }else if( total === 0 ){
+    componente = <Mensaje />
+  } else {
+    componente = <Resultado 
+                    total = { total }
+                    plazo = { plazo }
+                    cantidad = { cantidad }
+                />
+  }
+
+  return (
+    <Fragment>
+      <Header 
+         titulo = "Cotizador de prestamos"
+      />
+
+      <div className = "container">
+        <Formulario 
+            cantidad = { cantidad }
+            guardarCantidad = { guardarCantidad }
+            plazo = { plazo }
+            guardarPlazo = { guardarPlazo }
+            guardarTotal = { guardarTotal }
+            guardarCargando = { guardarCargando }
+        />
+
+        <div className = "mensajes">
+          { componente } 
+        </div>
+        
+      </div>
+    </Fragment>
+  );
+}
+
+export default App;
---
+++ src/componentes/Resultado.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Resultado = ( { total, plazo, cantidad } ) => ( 
+        
+    <div className = "u-full-width resultado">
+        <h2>Resumen</h2>
+        <p>La cantidad solicitada es: $ { cantidad } </p>
+        <p>A pagar en { plazo } Meses</p>
+        <p>Su pago mensual es de: $ { ( total / plazo ).toFixed(2) }</p>
+        <p>Total a pagar: $ { (total).toFixed(2) } Meses</p>
+    </div>
+);
+
+export default Resultado;
